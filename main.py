6c040a53131b16899a646d1f9fd14ab892b0c442
Daniel Chuma <83743774+dchuma@users.noreply.github.com>
2022-08-03 15:18:23 +0000

    replaced hello world with input exercises of episode two with networkchuck channel

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-print("""I am Iron Man,
-No, I am Tony Stark.
-No, I am poppy!
-""")
-# concatenate
-print("I am Iron Man! \n" + "No, I am Tony Stark! \n" + "No, I am poppy!")
+# Coffee shop using new python programming
+# concepts
+# print("Hello, welcome to DC-CoffeeShop")
+
+# print("What is your name?")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + " ,thank you so much for coming in today!\n\n\n")
+
+menu = "Black Coffee, Espresso, Latte, Cappucino"
+
+print(
+    name +
+    ", what would you like from our menu today? Here is what we are serving.\n"
+    + menu)
+
+order = input()
+
+print("Sounds good " + name + ", we will have that" + order +
+      "ready for you in a moment")
